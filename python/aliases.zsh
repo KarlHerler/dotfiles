907bee75541fae6727530a786c3d589e551b16c3
Karl Herler <hi@karlherler.com>
2013-02-27 15:41:41 +0200

    Added .DS_Store to the gitignore.

---
+++ python/aliases.zsh
@@ -0,0 +1,3 @@
+alias py="python"                                                                    
+alias pyvenv="source ./venv/bin/activate"
+alias serve="python -m SimpleHTTPServer"    # not really a python shortcut but it serves the current directory as a webserver 
