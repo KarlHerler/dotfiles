31a7edb4d862afad80c38d50e69d59086fefbadd
Karl Herler <hi@karlherler.com>
2013-02-27 19:11:11 +0200

    Added my horribly messy PATH.

---
+++ system/path.zsh
@@ -0,0 +1,3 @@
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/sbin:/usr/local/git/bin:/Users/kalle/.rvm/gems/ruby-1.9.2-p290/bin:/Users/kalle/.rvm/gems/ruby-1.9.2-p290@global/bin:/Users/kalle/.rvm/rubies/ruby-1.9.2-p290/bin:/Users/kalle/.rvm/bin:/opt/local/bin:/opt/local/sbin:/Users/kalle/scala/bin
+export PATH=/usr/local/bin:$PATH
+
