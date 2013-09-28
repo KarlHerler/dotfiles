06ef32ac37da8c12e5287904c753de3c81877827
Karl Herler <hi@karlherler.com>
2013-09-28 15:03:32 +0300

    Tiny fix to path.

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/X11/bin:/usr/local/sbin:/usr/local/git/bin:/Users/kalle/.rvm/gems/ruby-1.9.2-p290/bin:/Users/kalle/.rvm/gems/ruby-1.9.2-p290@global/bin:/Users/kalle/.rvm/rubies/ruby-1.9.2-p290/bin:/Users/kalle/.rvm/bin:/opt/local/bin:/opt/local/sbin:/Users/kalle/scala/bin
-export PATH=/usr/local/share/npm:$PATH #npm
+export PATH=/usr/local/share/npm/bin:$PATH #npm
 export PATH=/usr/local/bin:$PATH
 
